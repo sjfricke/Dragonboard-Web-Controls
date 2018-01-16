1dd395394bcb9530a137af86e8d240c7d3934185
sjfricke <sjfricke@wisc.edu>
2018-01-16 01:51:45 -0800

    working front end

--- website/src/main.js
+++ website/src/main.js
@@ -14,6 +14,62 @@ function setWebSocket() {
     }
 }
 
+var cpu_option;
+
 function setup() {
-    
+
+    // Starting values for CPU usage
+    cpu_option = {
+        animationEnabled: false,
+        title: {
+            text: "Dragonboard CPU Usage"
+        },
+        data: [{
+            type: "doughnut",
+            innerRadius: "50%",
+            legendText: "{label}",
+            indexLabel: "{label}: {y}%",
+            toolTipContent: "{label}: {y}%",
+            dataPoints: [
+                { label: "CPU 0", y: 50 },
+                { label: "CPU 1", y: 50 },
+                { label: "CPU 2", y: 50 },
+                { label: "CPU 3", y: 50 }
+            ]
+        }]
+    };
+    $("#chart-cpu").CanvasJSChart(cpu_option);
+
+    // keeps watch if we manually change value of GPIO from UI
+    $('.gpio').click(function(event) {
+        if (event.originalEvent) {
+            // if they click the element, but not button need to prevent a double flip
+            event.stopPropagation();
+        }
+    })
+
 }
+
+// Gets percetage of 4 cpus and sets it to be drawn on graph
+function updateCPU(c0, c1, c2, c3) {
+    var cpuData = cpu_option.data[0].dataPoints;
+    cpuData[0].y = c0;
+    cpuData[1].y = c1;
+    cpuData[2].y = c2;
+    cpuData[3].y = c3;
+    $("#chart-cpu").CanvasJSChart(cpu_option); // This rebinds the values
+}
+
+// gpio should be int of GPIO pin
+// value is either false or true
+function updateGPIO(gpio, value) {
+    $('.gpio' + gpio).prop('checked', value).change();
+}
+
+// set clear to clear list, otherwise list name to add
+function updateWiFi(name, clear) {
+    if (clear) {
+        $('#wifi')[0].innerHTML = "";
+    } else {
+        $('#wifi')[0].innerHTML += "<li class='list-group-item'>" + name + "</li>";
+    }}
--- website/src/websocket.js
+++ website/src/websocket.js
@@ -8,12 +8,22 @@ function wsOnMessage(event) {
   var message = JSON.parse(event.data);
 
   switch(parseInt(message.type)) {
-  case 1:
+  case 1: // CPU %
+      // data is packed as "20 30 30 60" string with % spaced out
+      var cpuList = message.value.split(" ");
+      updateCPU(cpuList[0], cpuList[1], cpuList[2], cpuList[3]);
       break;
-  case 2:
+  case 2: // GPIO  
+    var gpioMsg = message.value.split(" ");
+    var toggle = (gpioMsg[1] == 'false') ? false : true;
+    updateGPIO(gpioMsg[0], toggle);
     break;
-  case 3:
-      break;
+  case 3: // WiFi clear
+    updateWiFi(null, true);
+    break;
+  case 4: // WiFi add
+    updateWiFi(message.value);
+    break;
   default:
 	  warn("WebSocket", "No case for data: %0", message);
   }
@@ -26,6 +36,19 @@ function wsAllReadyToStart() {
 /////////////////////////////////////
 // for testing to callback echo ws //
 /////////////////////////////////////
-function test0() {
-    webSocket.send('{"type":0,"value":0}');
+function testCPU(c0, c1, c2, c3) {
+    var testString = c0 + " " + c1 + " " + c2 + " " + c3;
+    webSocket.send('{"type":1,"value":"' + testString + '"}');
+}
+
+function testGPIO(gpio, value) {    
+    webSocket.send('{"type":2,"value":"' + gpio + " " + value + '"}');
+}
+
+function testWiFiClear() {    
+    webSocket.send('{"type":3,"value":0}');
+}
+
+function testWiFiName(name) {    
+    webSocket.send('{"type":4,"value":"' + name + '"}');
 }
